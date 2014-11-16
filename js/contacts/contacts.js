4890c0686e6b81b762f1955e4f90a9845ba50fb4
Viktor Kukurba <victor.kukurba@sethq.com>
2014-11-16 18:30:19 +0200

    Used ui router insteadof ngRoute, added moneyua form

--- js/contacts/contacts.js
+++ js/contacts/contacts.js
@@ -1,55 +1,58 @@
 'use strict';
 
-angular.module('fredra.contacts', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/contacts', {
-            templateUrl: 'js/contacts/index.html',
-            controller: 'contactsController'
-        });
-    }])
-    .controller('contactsController', ['$scope','fbPageData', function($scope, fbPageData) {
+angular.module('fredra.contacts', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider',
+      function($stateProvider) {
+        $stateProvider
+            .state('contacts', {
+              templateUrl: 'js/contacts/index.html',
+              url: '/contacts',
+              controller: function($scope, fbPageData) {
 
-    /**
-     * Watch for Facebook to be ready.
-     * There's also the event that could be used
-     */
-  $scope.$watch(
-    function() {
-      return fbPageData.isReady();
-    },
-    function(newVal) {
-      if (newVal)
-        $scope.facebookReady = true;
-    }
-  );
+                /**
+                 * Watch for Facebook to be ready.
+                 * There's also the event that could be used
+                 */
+                $scope.$watch(
+                    function() {
+                      return fbPageData.isReady();
+                    },
+                    function(newVal) {
+                      if (newVal)
+                        $scope.facebookReady = true;
+                    }
+                );
 
-    if (fbPageData.isLogged()) {
-      renderFBContent(fbPageData.getPageData());
-    } else {
-      fbPageData.setCallback(renderFBContent);
-    }
+                if (fbPageData.isLogged()) {
+                  renderFBContent(fbPageData.getPageData());
+                } else {
+                  fbPageData.setCallback(renderFBContent);
+                }
 
-    function renderFBContent(data) {
-      $scope.logged = true;
-      $scope.location = data.location;
+                function renderFBContent(data) {
+                  $scope.logged = true;
+                  $scope.location = data.location;
 
-      var mapCanvas = document.getElementById('map_canvas');
-      var myLatlng = new google.maps.LatLng($scope.location.latitude, $scope.location.longitude);
-      var mapOptions = {
-        center: myLatlng,
-        zoom: 17,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(mapCanvas, mapOptions);
-      // To add the marker to the map, use the 'map' property
-      var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title:"Fredra.61"
-      });
-    }
+                  var mapCanvas = document.getElementById('map_canvas');
+                  var myLatlng = new google.maps.LatLng($scope.location.latitude, $scope.location.longitude);
+                  var mapOptions = {
+                    center: myLatlng,
+                    zoom: 17,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                  };
+                  var map = new google.maps.Map(mapCanvas, mapOptions);
+                  // To add the marker to the map, use the 'map' property
+                  var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    map: map,
+                    title: 'Fredra.61'
+                  });
+                }
 
-    $scope.IntentLogin = function() {
-      fbPageData.intentLogin();
-    };
-    }]);
+                $scope.IntentLogin = function() {
+                  fbPageData.intentLogin();
+                };
+              }
+            });
+      }
+    ]);
--- js/donation/donation.js
+++ js/donation/donation.js
@@ -1,48 +1,83 @@
 'use strict';
 
-angular.module('fredra.donation', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/donation/:service?', {
-            templateUrl: 'js/donation/index.html',
-            controller: 'donationController'
-        });
-    }])
-    .controller('donationController', ['$scope', '$location', '$routeParams', '$compile', '$templateCache', 'Facebook', function($scope, $location, $routeParams, $compile,  $templateCache, Facebook ) {
-    var tabs = [
-      { link : '#/donation/privatbank', label : 'Приватбанк', name: 'privatbank' },
-      { link : '#/donation/moneyua', label : 'Money UA', name: 'moneyua' },
-      { link : '#/donation/easypayua', label : 'Easypay UA', name: 'easypayua' }
-    ];
+angular.module('fredra.donation', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider',
+      function($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
+            .state('donation', {
+              templateUrl: 'js/donation/index.html',
+              url: '/donation',
+              controller: function($scope, $location, Facebook) {
+                var tabs = [
+                  {
+                    link: '#/donation/privatbank',
+                    label: 'Приватбанк',
+                    name: 'privatbank'
+                  },
+                  {
+                    link: '#/donation/moneyua',
+                    label: 'Money UA',
+                    name: 'moneyua'
+                  },
+                  {
+                    link: '#/donation/easypayua',
+                    label: 'Easypay UA',
+                    name: 'easypayua'
+                  }
+                ];
 
+                tabViewHandler($scope, $location, tabs);
+              }
+            })
+            .state('donation.privatbank', {
+              templateUrl: 'js/donation/privatbank-form.html',
+              url: '/privatbank',
+              controller: function($scope, generateCode, pbRequestData) {
+                $scope.pbOrder = generateCode.getCode();
+                if (pbRequestData.isInit()) {
+                  var config = pbRequestData.getPbConfig();
+                  setData(config);
+                } else {
+                  pbRequestData.setCallback(function(config) {
+                    setData(config);
+                  });
+                }
+                function setData(config) {
+                  $scope.pbMerchant = config.merchant;
+                  $scope.pbDetails = config.details;
+                  $scope.return_url = config.return_url;
+                  $scope.pbExt_details = config.ext_details;
+                  $scope.defaultAmount = config.defaultAmount;
+                }
+              }
+            }).state('donation.moneyua', {
+              templateUrl: 'js/donation/moneyua-form.html',
+              url: '/moneyua',
+              controller: function($scope, moneyuaRequestData) {
+                if (moneyuaRequestData.isInit()) {
+                  var config = moneyuaRequestData.getPbConfig();
+                  setData(config);
+                } else {
+                  moneyuaRequestData.setCallback(function(config) {
+                    setData(config);
+                  });
+                }
+                function setData(config) {
+                  $scope.step = config.step;
+                  $scope.okpo = config.okpo;
+                  $scope.acc = config.acc;
+                  $scope.name = config.name;
+                  $scope.defaultAmount = config.defaultAmount;
+                  $scope.mfo = config.mfo;
+                  $scope.info = config.info;
+                }
+              }
+            }).state('donation.easypayua', {
+              templateUrl: 'js/donation/easypayua-form.html',
+              url: '/easypayua',
+              controller: function() {
+              }
+            });
+      }]);
 
-    tabViewHandler($scope, $location, tabs);
-
-    var formTemplate = $templateCache.get($routeParams.service + '-form');
-    $('#donation-container')
-      .html('')
-      .append($compile(formTemplate)($scope));
-    Facebook.parseXFBML(document.querySelector('.jumbotron'));
-    }]).controller('privatbankController', ['$scope', 'generateCode', 'pbRequestData', function($scope, generateCode, pbRequestData) {
-    $scope.pbOrder = generateCode.getCode();
-    if (pbRequestData.isInit()) {
-      var config = pbRequestData.getPbConfig();
-      $scope.pbMerchant = config.merchant;
-      $scope.pbDetails = config.details;
-      $scope.return_url = config.return_url;
-      $scope.pbExt_details = config.ext_details;
-      $scope.defaultAmount = config.defaultAmount;
-    } else {
-      pbRequestData.setCallback(function(pdConfig){
-        $scope.pbMerchant = pdConfig.merchant;
-        $scope.pbDetails = pdConfig.details;
-        $scope.return_url = pdConfig.return_url;
-        $scope.pbExt_details = pdConfig.ext_details;
-        $scope.defaultAmount = pdConfig.defaultAmount;
-      });
-    }
-  }]).controller('moneyuaController', [function(){
-
-  }]).controller('easypayuaController', [function(){
-
-  }]);
