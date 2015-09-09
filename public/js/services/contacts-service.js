83663936ff4f12a29a191c06b8612d2a307a5260
Viktor Kukurba <victor.kukurba@sethq.com>
2015-09-10 02:17:45 +0300

    update contacts page

--- public/js/services/contacts-service.js
+++ public/js/services/contacts-service.js
@@ -5,7 +5,7 @@ define([
   'use strict';
   angular.module('lidikArt').factory('contactsData', [function () {
     var data = {
-      massage: 'Звертайтеся з цікавими пропозиціями та замовленнями.',
+      message: 'Звертайтеся з цікавими пропозиціями та замовленнями.',
       email: 'lidik@list.ru',
       mobile: '0961721752',
       fb: 'https://www.facebook.com/pages/Lidikart/1506114906267456',
