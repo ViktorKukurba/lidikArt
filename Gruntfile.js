91aa14393f84130f3d28ddcd3126e103c8ef4d2f
Viktor Kukurba <victor.kukurba@sethq.com>
2018-02-22 02:09:04 +0200

    Update sass dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
 
   grunt.registerTask('default', ['jshint', 'requirejs', 'sass', 'watch']);
 
